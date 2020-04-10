4bf9e7bdbb95fa68e2b9770f98ec04bfdddd4a5d
Hyunchel Kim <hyunchel.inbox@gmail.com>
2020-04-09 22:06:45 -0500

    bin: venv

--- bin/functions/misc.sh
+++ bin/functions/misc.sh
@@ -1,10 +1,18 @@
 #! /usr/bin/env bash
-alias cdea="conda deactivate"
 
+# conda
+alias cdea="conda deactivate"
 cact() {
     conda activate $(head environment.yml | grep 'name:' | awk '{print $2}')
 }
 
+# venv
+alias vdea="deactivate"
+vact() {
+    source ./venv/bin/activate
+}
+
+# misc
 ff() {
     grep -Iinr --color=always --exclude=*~ --exclude='./htmlcov/*' --exclude-dir='.mypy*' --exclude-dir='.build' --exclude='tags' "$1" .
 }
