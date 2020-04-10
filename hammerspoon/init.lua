f7da169f7ed0ba366e73e5acddda4ba861680bf1
Hyunchel Kim <hyunchel.inbox@gmail.com>
2020-04-09 22:03:55 -0500

    hammerspoon: add spoon

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -1,5 +1,9 @@
+-- spoons
+hs.loadSpoon("ReloadConfiguration")
+spoon.ReloadConfiguration:start()
+
 -- resize window to left/right half.
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "h", function()
     local win = hs.window.focusedWindow()
     local f = win:frame()
     local screen = win:screen()
@@ -12,7 +16,7 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Left", function()
     win:setFrame(f)
 end)
 
-hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "l", function()
     local win = hs.window.focusedWindow()
     local f = win:frame()
     local screen = win:screen()
@@ -23,4 +27,17 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "Right", function()
     f.w = max.w / 2
     f.h = max.h
     win:setFrame(f)
+end)
+
+-- moods
+-- main
+hs.hotkey.bind({"cmd", "alt", "ctrl"}, "m", function()
+    local laptopScreen = "LG HDR 4K"
+    local windowLayout = {
+        {"Chrome", nil, laptopScreen, hs.layout.left50, nil, nil},
+        {"Terminal", nil, laptopScreen, hs.layout.right50, nil, nil},
+        {"Spotify", nil, laptopScreen, hs.layout.maximized, nil, nil},
+        {"Leaf", nil, laptopScreen, hs.layout.maximized, nil, nil},
+    }
+    hs.layout.apply(windowLayout)
 end)
