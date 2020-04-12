5c55fd497d177ad158eb762bebce631fe5160441
Hyunchel Kim <hyunchel.inbox@gmail.com>
2020-04-11 21:00:18 -0500

    hammerspoon: add URL events

--- hammerspoon/init.lua
+++ hammerspoon/init.lua
@@ -41,3 +41,35 @@ hs.hotkey.bind({"cmd", "alt", "ctrl"}, "m", function()
     }
     hs.layout.apply(windowLayout)
 end)
+
+-- URLs
+hs.urlevent.bind("pomodoro", function(eventName, params)
+    hs.notify.new({
+        title="Starting pomodoro now.",
+        informativeText="[pomodoro] 25 min session."
+    }):send()
+    hs.timer.doAfter(3, function()
+        hs.notify.new({
+            title="Time's up!",
+            informativeText="[pomodoro] Take a break."
+        }):send()
+    end)
+end)
+
+hs.urlevent.bind("reminder", function(eventName, params)
+    what = params["what"]
+    minutes = params["minutes"]
+    notifyItem = {
+        title='I will remind you soon',
+        informativeText="Counting " .. minutes .. " min now",
+    }
+    hs.notify.new(notifyItem):send()
+
+    hs.timer.doAfter(tonumber(minutes) * 60, function()
+        notifyItem = {
+            title=what,
+            informativeText=minutes .. " min past!",
+        }
+        hs.notify.new(notifyItem):send()
+    end)
+end)
