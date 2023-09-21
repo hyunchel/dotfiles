f94ec8828b9f6d2b1d8e2bf09cefbc2bef88138f
Hyunchel Kim <hyunchel.inbox@gmail.com>
2023-09-20 22:49:33 -0400

    nvim: change colorscheme

--- config/nvim/lua/hyunchel/packer.lua
+++ config/nvim/lua/hyunchel/packer.lua
@@ -8,13 +8,15 @@ return require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'
 
   use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.1',
+      'nvim-telescope/telescope.nvim', tag = '0.1.3',
       -- or                            , branch = '0.1.x',
       requires = { {'nvim-lua/plenary.nvim'} }
   }
 
   use({ 'rose-pine/neovim', as = 'rose-pine' })
-  vim.cmd('colorscheme rose-pine')
+  -- vim.cmd('colorscheme rose-pine')
+  use('EdenEast/nightfox.nvim')
+  vim.cmd('colorscheme nightfox')
 
   use('nvim-treesitter/nvim-treesitter', { run = 'TSUpdate' })
   use("nvim-treesitter/playground")
