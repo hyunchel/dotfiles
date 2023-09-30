0804d4a7954ae36ef28b5f824fe32085d686fbe6
Hyunchel Kim <hyunchel.inbox@gmail.com>
2023-09-30 12:51:42 -0400

    lsp-zero: migrate to v3.x

--- config/nvim/after/plugin/lsp.lua
+++ config/nvim/after/plugin/lsp.lua
@@ -1,38 +1,39 @@
-local lsp = require('lsp-zero').preset({
+local lsp_zero = require('lsp-zero').preset({
     name = 'recommended',
 })
 
+-- cmp
 local cmp = require('cmp')
-local cmp_select = { behavior = cmp.SelectBehavior.Select }
-local cmp_mappings = lsp.defaults.cmp_mappings({
-	['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-	['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
-	['<C-y>'] = cmp.mapping.confirm({ select = true }),
-	['<C-Space>'] = cmp.mapping.complete(),
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+    mapping = cmp.mapping.preset.insert({
+        ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+        ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+    })
 })
 
-lsp.setup_nvim_cmp({
-	mappings = cmp_mappings,
-})
+lsp_zero.on_attach(function(client, bufnr)
+    lsp_zero.default_keymaps({
+        buffer = bufnr,
+        exclude = { '<F2>', '<F3>', '<F4>' },
+    })
 
-lsp.on_attach(function(client, bufnr)
-  lsp.default_keymaps({
-      buffer = bufnr,
-      exclude = {'<F2>', '<F3>', '<F4>'},
-  })
-
-  -- custom bindings
-  vim.keymap.set("n", ",gr", vim.lsp.buf.rename)
-  vim.keymap.set("n", ",ga", vim.lsp.buf.code_action)
-  vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
-  vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
-  -- XXX: or we could auto format on save?
-  vim.keymap.set("n", ",gf", vim.lsp.buf.format)
-  vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-  vim.keymap.set("n", ".,", vim.lsp.buf.document_symbol)
+    -- custom bindings
+    vim.keymap.set("n", "gc", vim.lsp.buf.incoming_calls)
+    vim.keymap.set("n", ",gr", vim.lsp.buf.rename)
+    vim.keymap.set("n", ",ga", vim.lsp.buf.code_action)
+    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename)
+    vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action)
+    -- XXX: or we could auto format on save?
+    vim.keymap.set("n", ",gf", vim.lsp.buf.format)
+    vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+    vim.keymap.set("n", ".,", vim.lsp.buf.document_symbol)
 end)
 
-lsp.set_sign_icons = ({
+lsp_zero.set_sign_icons = ({
     error = '✘',
     warn = '▲',
     hint = '⚑',
@@ -42,21 +43,22 @@ lsp.set_sign_icons = ({
 -- mason
 require('mason').setup({})
 require('mason-lspconfig').setup({
-  ensure_installed = {
-    'tsserver',
-    'rust_analyzer',
-    'lua_ls',
-    'gopls',
-    'hls',
-  },
-  handlers = {
-    lsp.default_setup,
-  }
+    ensure_installed = {
+        'tsserver',
+        'rust_analyzer',
+        'lua_ls',
+        'gopls',
+        'hls',
+    },
+    handlers = {
+        lsp_zero.default_setup,
+        lua_ls = function()
+            -- lua specific settings
+            local lua_opts = lsp_zero.nvim_lua_ls()
+            require('lspconfig').lua_ls.setup(lua_opts)
+        end
+    }
 })
 
--- lua specific settings
-local lua_opts = lsp.nvim_lua_ls()
-require('lspconfig').lua_ls.setup(lua_opts)
-
 -- setup
-lsp.setup()
+lsp_zero.setup()
--- config/nvim/lua/hyunchel/packer.lua
+++ config/nvim/lua/hyunchel/packer.lua
@@ -4,46 +4,38 @@
 vim.cmd.packadd('packer.nvim')
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  use {
-      'nvim-telescope/telescope.nvim', tag = '0.1.3',
-      -- or                            , branch = '0.1.x',
-      requires = { {'nvim-lua/plenary.nvim'} }
-  }
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.3',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
 
-  use('EdenEast/nightfox.nvim')
-  vim.cmd('colorscheme nightfox')
+    use('EdenEast/nightfox.nvim')
+    vim.cmd('colorscheme nightfox')
 
-  use('nvim-treesitter/nvim-treesitter', { run = 'TSUpdate' })
-  use("theprimeagen/harpoon")
-  use("mbbill/undotree")
-  use("tpope/vim-fugitive")
-  use {
-      'VonHeikemen/lsp-zero.nvim',
-      branch = 'v2.x',
-      requires = {
-          -- LSP Support
-          {'neovim/nvim-lspconfig'},             -- Required
-          {                                      -- Optional
-              'williamboman/mason.nvim',
-              run = function()
-                  pcall(vim.cmd, 'MasonUpdate')
-              end,
-          },
-      {'williamboman/mason-lspconfig.nvim'}, -- Optional
+    use('nvim-treesitter/nvim-treesitter', { run = 'TSUpdate' })
+    use("theprimeagen/harpoon")
+    use("mbbill/undotree")
+    use("tpope/vim-fugitive")
 
-      -- Autocompletion
-      {'hrsh7th/nvim-cmp'},     -- Required
-      {'hrsh7th/cmp-nvim-lsp'}, -- Required
-      {'L3MON4D3/LuaSnip'},     -- Required
-  }
+    use { 'VonHeikemen/lsp-zero.nvim', branch = 'v3.x' }
 
-}
+    -- LSP support
+    use('neovim/nvim-lspconfig')
+    use('williamboman/mason.nvim')
+    use('williamboman/mason-lspconfig.nvim')
 
-  use("github/copilot.vim")
-  use("vimwiki/vimwiki")
-  use("junegunn/fzf", {["do"] = function() vim.fn["fzf#install"]() end})
-  use("junegunn/fzf.vim")
+    -- Autocompletion
+    use('hrsh7th/nvim-cmp')
+    use('hrsh7th/cmp-nvim-lsp')
+    use('L3MON4D3/LuaSnip')
+
+    -- Rest
+    use("github/copilot.vim")
+    use("vimwiki/vimwiki")
+    use("junegunn/fzf", { ["do"] = function() vim.fn["fzf#install"]() end })
+    use("junegunn/fzf.vim")
 end)
