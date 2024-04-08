080bd16b3b0ecc97ca1940effd72e9c109d8d948
Hyunchel Kim <hyunchel.inbox@gmail.com>
2024-04-08 16:49:10 +0000

    config: add linux configs

---
+++ config/hypr/scripts/change-wallpaper.sh
@@ -0,0 +1,7 @@
+#!/bin/zsh
+# Set a random wallpaper from a directory
+find ~/wallpapers/5120x1440/ -type f -name '*.jpg' -o -name '*.png' | shuf | tail -n 1 | xargs swww img \
+    --transition-type wipe --transition-fps 120 --transition-duration 3.0 --transition-step 90 --transition-angle 30
+
+# Update terminal colors
+swww query|awk '{print $NF}'|xargs wal -i
---
+++ config/hypr/scripts/update-colors.sh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+# Update hyprland colors
+cp ~/.cache/wal/colors-hyprland ~/.config/hypr/colors.conf
+
+# Update firefox theme
+pywalfox update
