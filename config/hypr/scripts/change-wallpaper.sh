cb43d072f26323d2be320572690084071813905e
Hyunchel Kim <hyunchel.inbox@gmail.com>
2025-03-06 16:08:49 -0500

    hyprland: update

--- config/hypr/scripts/change-wallpaper.sh
+++ config/hypr/scripts/change-wallpaper.sh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 # Set a random wallpaper from a directory
+# find ~/wallpapers/3840x2160/ -type f -name '*.jpg' -o -name '*.png' | shuf | tail -n 1 | xargs swww img \
 find ~/wallpapers/5120x1440/ -type f -name '*.jpg' -o -name '*.png' | shuf | tail -n 1 | xargs swww img \
     --transition-type wipe --transition-fps 120 --transition-duration 3.0 --transition-step 90 --transition-angle 30
 
