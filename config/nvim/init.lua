c186f76ce8c32f528c3488659bc96aa9a8acaad4
Hyunchel Kim <hyunchel.inbox@gmail.com>
2025-05-17 22:54:53 -0400

    nvim: add shortcuts for quickfix list

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -75,7 +75,7 @@ vim.keymap.set("n", "<leader>se", vim.cmd.Ex)
 vim.keymap.set("n", "<Esc>", "<cmd>nohlsearch<CR>")
 
 -- Diagnostic keymaps
-vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostic [Q]uickfix list" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setqflist, { desc = "Open diagnostic [Q]uickfix list" })
 vim.keymap.set("n", "gl", vim.diagnostic.open_float, { desc = "Open diagnostic f[L]oat" })
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
@@ -112,8 +112,9 @@ vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" }
 -- vim.keymap.set("n", "Q", "<nop>")
 --
 -- -- debatable
--- vim.keymap.set("n", "<C-n>", "<cmd>cnext<CR>zz")
--- vim.keymap.set("n", "<C-p>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-n>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-p>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<C-q>", "<cmd>cclose<CR>zz")
 -- vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 -- vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
